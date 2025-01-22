8d7ffeb0ca3eca9f44d3f3e2d598312f84e2bac6
konnta0 <konnta0@gmail.com>
2025-01-23 00:49:02 +0900

    fix

--- AspireApp1/AspireApp1.AppHost/Program.cs
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -3,10 +3,10 @@ var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice"
 
 // https://github.com/SigNoz/signoz/blob/v0.68.0/deploy/docker/clickhouse-setup/docker-compose-minimal.yaml
 var zookeeper = builder.AddContainer("signoz-zookeeper", "bitnami/zookeeper", "3.7.1")
-    .WithEndpoint(2181, 2181)
-    .WithEndpoint(2888, 2888)
-    .WithEndpoint(3888, 3888)
-    .WithVolume("/data/zookeeper", "/bitnami/zookeeper")
+    .WithEndpoint(2181, name: "zookeeper-2181", isProxied: false)
+    .WithEndpoint(2888, name: "zookeeper-2888", isProxied: false)
+    .WithEndpoint(3888, name: "zookeeper-3888", isProxied: false)
+    .WithBindMount("Container/data/zookeeper", "/bitnami/zookeeper")
     .WithEnvironment("ALLOW_ANONYMOUS_LOGIN", "yes")
     .WithEnvironment("ZOO_SERVER_ID", "1")
     .WithEnvironment("ZOO_AUTOPURGE_INTERVAL", "1");
