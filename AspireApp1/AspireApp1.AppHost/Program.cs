f0c8eb85ec149407301557ea0955fdd8d1c0da87
konnta0 <konnta0@gmail.com>
2025-01-24 00:33:59 +0900

    start containers

--- AspireApp1/AspireApp1.AppHost/Program.cs
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -1,8 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
-var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
 
 // https://github.com/SigNoz/signoz/blob/v0.68.0/deploy/docker/clickhouse-setup/docker-compose-minimal.yaml
-var zookeeper = builder.AddContainer("signoz-zookeeper", "bitnami/zookeeper", "3.7.1")
+var zookeeper = builder.AddContainer("zookeeper-1", "bitnami/zookeeper", "3.7.1")
     .WithEndpoint(2181, name: "zookeeper-2181", isProxied: false)
     .WithEndpoint(2888, name: "zookeeper-2888", isProxied: false)
     .WithEndpoint(3888, name: "zookeeper-3888", isProxied: false)
@@ -11,7 +10,7 @@ var zookeeper = builder.AddContainer("signoz-zookeeper", "bitnami/zookeeper", "3
     .WithEnvironment("ZOO_SERVER_ID", "1")
     .WithEnvironment("ZOO_AUTOPURGE_INTERVAL", "1");
 
-var clickhouse = builder.AddContainer("signoz-clickhouse", "clickhouse/clickhouse-server", "24.1.2-alpine")
+var clickhouse = builder.AddContainer("clickhouse", "clickhouse/clickhouse-server", "24.1.2-alpine")
     .WithEndpoint(8123, name: "clickhouse-8123", isProxied: false)
     .WithEndpoint(9000, name: "clickhouse-9000", isProxied: false)
     .WithEndpoint(9181, name: "clickhouse-9181", isProxied: false)
@@ -23,22 +22,24 @@ var clickhouse = builder.AddContainer("signoz-clickhouse", "clickhouse/clickhous
     .WithBindMount("Container/data/clickhouse/", "/var/lib/clickhouse/")
     .WaitFor(zookeeper);
 
-var queryFrontEnd = builder.AddContainer("signoz-frontend", "signoz/frontend", "0.68.0")
-    .WithHttpEndpoint(3301, name: "frontend-3301", isProxied: false)
-    .WithBindMount("Container/config/frontend/nginx-config.conf", "/etc/nginx/conf.d/default.conf");
-
-var otelMigratorSync = builder.AddContainer("otel-migrator-sync", "signoz/signoz-schema-migrator", "0.111.23")
-    .WithArgs("sync", "--dsn=tcp://host.docker.iternal:9000", "--up=")
+var otelMigratorSync = builder.AddContainer("otel-collector-migrator-sync", "signoz/signoz-schema-migrator", "0.111.23")
+    .WithArgs("sync", "--dsn=tcp://clickhouse:9000", "--up=")
+    .WithReference(clickhouse.GetEndpoint("clickhouse-9000"))
     .WaitFor(clickhouse);
 
-var queryService = builder.AddContainer("signoz-query-service", "signoz/query-service", "0.68.0")
+var otelMigratorAsync = builder.AddContainer("otel-collector-migrator-async", "signoz/signoz-schema-migrator", "0.111.23")
+    .WithArgs("async", "--dsn=tcp://clickhouse:9000", "--up=")
+    .WithReference(clickhouse.GetEndpoint("clickhouse-9000"))
+    .WaitForCompletion(otelMigratorSync, 0);
+
+var queryService = builder.AddContainer("query-service", "signoz/query-service", "0.68.0")
     .WithArgs("-config=/root/config/prometheus.yml", "--use-logs-new-schema=true",
         "--use-trace-new-schema=true")
     .WithBindMount("Container/config/queryservice/prometheus.yml", "/root/config/prometheus.yml")
     .WithBindMount("Container/config/queryservice/dashboards", "/root/config/dashboards")
     .WithBindMount("Container/data/queryservice/", "/var/lib/signoz/")
-    .WithEnvironment("ClickHouseUrl", "tcp://host.docker.internal:9000")
-    .WithEnvironment("ALERTMANAGER_API_PREFIX", "http://host.docker.internal:9093/api/")
+    .WithEnvironment("ClickHouseUrl", "tcp://clickhouse:9000")
+    .WithEnvironment("ALERTMANAGER_API_PREFIX", "http://alertmanager:9093/api/")
     .WithEnvironment("SIGNOZ_LOCAL_DB_PATH", "/var/lib/signoz/signoz.db")
     .WithEnvironment("DASHBOARDS_PATH", "/root/config/dashboards")
     .WithEnvironment("STORAGE", "clickhouse")
@@ -46,34 +47,53 @@ var queryService = builder.AddContainer("signoz-query-service", "signoz/query-se
     .WithEnvironment("TELEMETRY_ENABLED", "true")
     .WithEnvironment("DEPLOYMENT_TYPE", "docker-standalone-amd")
     .WithHttpEndpoint(8085, name: "query-service-8085", isProxied: false)
+    .WithReference(clickhouse.GetEndpoint("clickhouse-9000"))
     .WaitFor(clickhouse)
     .WaitFor(otelMigratorSync);
 
-var alertManager = builder.AddContainer("signoz-alertmanger", "signoz/alertmanager", "0.23.7")
+var frontend = builder.AddContainer("frontend", "signoz/frontend", "0.68.0")
+    .WithHttpEndpoint(3301, name: "frontend-3301", isProxied: false)
+    .WithBindMount("Container/config/frontend/nginx-config.conf", "/etc/nginx/conf.d/default.conf")
+    .WithReference(queryService.GetEndpoint("query-service-8085"))
+    .WaitFor(queryService);
+
+var alertManager = builder.AddContainer("alertmanger", "signoz/alertmanager", "0.23.7")
     .WithArgs("--queryService.url=http://query-service:8085", "--storage.path=/data")
     .WithEndpoint(9093, name: "alertmanager-9093", isProxied: false)
     .WithBindMount("Container/data/alertmanager", "/data")
     .WithReference(queryService.GetEndpoint("query-service-8085"))
+    .WaitFor(queryService)
     .WaitFor(clickhouse);
 
-var otelCollector = builder.AddContainer("otel-collector", "ignoz/signoz-otel-collector", "0.111.23")
+queryService.WithReference(alertManager.GetEndpoint("alertmanager-9093"));
+
+var otelCollector = builder.AddContainer("otel-collector", "signoz/signoz-otel-collector", "0.111.23")
     .WithArgs("--config=/etc/otel-collector-config.yaml", "--manager-config=/etc/manager-config.yaml",
         "--copy-path=/var/tmp/collector-config.yaml", "--feature-gates=-pkg.translator.prometheus.NormalizeName")
     .WithBindMount("Container/config/otel-collector/otel-collector-config.yaml", "/etc/otel-collector-config.yaml")
     .WithBindMount("Container/config/otel-collector/otel-collector-opamp-config.yaml", "/etc/manager-config.yaml")
+    .WithBindMount("/var/lib/docker/containers", "/var/lib/docker/containers", true)
+    .WithBindMount("/", "/hostfs", true)
     .WithEnvironment("OTEL_RESOURCE_ATTRIBUTES", "host.name=signoz-host,os.type=linux")
     .WithEnvironment("LOW_CARDINAL_EXCEPTION_GROUPING", "false")
     .WithEndpoint(4317, name: "otel-collector-4317", isProxied: false)
-    .WithEndpoint(4318, name: "otel-collector-4318", isProxied: false)
+    .WithHttpEndpoint(4318, name: "otel-collector-4318", isProxied: false)
     .WaitFor(clickhouse)
     .WaitFor(queryService)
-    .WaitForCompletion(otelMigratorSync);
+    .WaitForCompletion(otelMigratorSync, 0);
+
+var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice")
+    .WithReference(otelCollector.GetEndpoint("otel-collector-4318"))
+    .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otel-collector:4318")
+    .WaitFor(otelCollector);
 
 builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(otelCollector.GetEndpoint("otel-collector-4318"))
+    .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otel-collector:4318")
     .WaitFor(apiService)
-    .WaitFor(queryFrontEnd)
+    .WaitFor(frontend)
     .WaitFor(otelCollector);
 
 builder.Build().Run();
